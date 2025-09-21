e7fd24c5842ec60540b643965808a81dbcea448a
Elijah <125945721+Elijah-05@users.noreply.github.com>
2025-09-21 06:53:46 +0000

    Implement Users Card Components

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -5,4 +5,15 @@ const Users = () => {
     </div>
 }
 
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
 export default Users
